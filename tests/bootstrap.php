fd664b78444c233c34ca33f14609315738293c90
王安宁 <wanguowan521@163.com>
2017-05-20 14:35:42 +0800

    add phpunit test example;

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wan
+ * Date: 5/20/17
+ * Time: 2:09 PM
+ */
+
+if ( file_exists( __DIR__ . '/../autoload.php' ) )
+{
+	require __DIR__ . '/../autoload.php';
+}
+
+require __DIR__ . '/TestBase.php';
